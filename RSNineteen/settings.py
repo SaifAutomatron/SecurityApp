c749e760d224ecc1919c0a13c94ef24a961b3d7f
Maxdev <wwe2k16flop@gmail.com>
2025-06-21 13:02:24 +0100

    added few pages

--- RSNineteen/settings.py
+++ RSNineteen/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '<your-secret-key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # Change to your domain/heroku app in production
+ALLOWED_HOSTS = ['rsnineteen-17f270f81e7a.herokuapp.com']  # Change to your domain/heroku app in production
 
 
 # Application definition
